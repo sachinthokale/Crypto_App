357af61b04ee1fc6b8f2759c1ae48602abcbc06d
Sachin Thokale <87713205+sachinthokale@users.noreply.github.com>
2022-12-14 13:31:21 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,37 @@
+import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { motion } from "framer-motion"
 
 function Home() {
     return (
-        <div>Home</div>
+
+
+
+        <Box bgColor={"blackAlpha.900"} w={"full"}>
+            <motion.div style={{
+                height: "80vh"
+            }}
+                animate={{
+                    translateY: "20px",
+
+                }}
+                transition={{
+                    duration: "2",
+                    repeat: "Infinity",
+                    repeatType: "reverse"
+                }}
+            >
+                <Image w={"full"} h={'600px'}
+                    filter={"grayscale(1)"} objectFit={"contain"} src={'bitcoin_home.png'}
+                />
+
+            </motion.div>
+
+            <Text fontSize={"6x1"} textAlign={"center"} fontWeight={"bold"}
+                color="whiteAlpha.700"
+                mt={-10}>  Xcrypto
+            </Text>
+        </Box>
     )
 }
 
